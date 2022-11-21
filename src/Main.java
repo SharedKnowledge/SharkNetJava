d31606ba2bf83d217b3065b37bcc20532dce9d92
thsc42 <thomas.schwotzer@htw-berlin.de>
2022-11-21 14:43:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Throw me away and replace me with messenger / hub UI software");
+    }
+}
---
+++ tests/DummyTests.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class DummyTests {
+    @Test
+    public void test1() {
+        // git does not create empty folder
+    }
+}
